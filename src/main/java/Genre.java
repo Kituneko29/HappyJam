eaffaeeebe4f6e3cc380ce8bd1cacc425070a5e0
pagopani <panipani4772@icloud.com>
2021-11-16 10:34:26 +0900

    camera.javaからgenre.javaに変更

--- src/main/java/Genre.java
+++ src/main/java/Genre.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Camera
  */
-@WebServlet("/Camera")
-public class Camera extends HttpServlet {
+@WebServlet("/Genre")
+public class Genre extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Camera() {
+    public Genre() {
         super();
         // TODO Auto-generated constructor stub
     }
